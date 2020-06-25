ea671c02c18a3d84bf24d6c0f9efee59a606b148
DanielBusarello <62729531+DanielBusarello@users.noreply.github.com>
2020-06-25 13:36:10 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,38 +11,3 @@ app.use(routes);
 app.use(errors());
 
 module.exports = app;  //app.listen(port) vai escutar uma porta do localhost no navegador
-
-/**
- * Rota / Recurso
- * localhost:3333 = Rota
- * localhost:3333/users = Recurso
- */
-
- /**
-  * Métodos HTTP
-  * GET: Buscar/listar uma informação do back-end
-  * POST: Criar uma informação no back-end
-  * PUT: Alterar uma informação no back-end
-  * DELETE: Deletar uma informação no back-end
-  */
-
-  /**
-   * Tipos de parâmetros:
-   * 
-   * Query Params: Parâmetros nomeados enviados na rota após o "?" (filtros, paginação). (request.query para utilizar o Query)
-   * Route Params: Parâmetros utilizdos para identificar recursos. (request.params para utilizar o Route)
-   * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos. (request.body para utilizar o Request Body)
-   */
-
-  /**
-   * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsft SQL Server
-   * NoSQL: MongoDB, CouchDB, etc
-   */
-
-   /**
-    * Driver: (SELECT * FROM users)
-    * Query Builder: table('users').select('*').where()
-    */
-
-
-
